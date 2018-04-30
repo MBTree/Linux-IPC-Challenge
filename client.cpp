9a79f25312363d5f7024ebf92b8287e205882d99
MBTree <31641097+MBTree@users.noreply.github.com>
2018-04-30 14:24:23 -0500

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -30,38 +30,34 @@ int main(int argc, char **argv) {
    
    }
 
-   key = ftok(".", 'q');
    //Create the message queue
+   key = ftok(".", 'q');
    if( (qid = msgget(key, 0)) == -1) {
       perror("Failed to get message queue");
       exit(EXIT_FAILURE);
    }
 
-   
+   //Setup message buffer to be sent
    std::string message = std::string(argv[1]);
-
    strncpy(qbuf.mtext, message.c_str(), MAX_MSG_SIZE-10);
    qbuf.mtext[MAX_MSG_SIZE - 1] = '\0';
    qbuf.mtype = SND_TYPE;
 
+   //Send raw message to server
    if((msgsnd(qid, &qbuf, strlen(qbuf.mtext)+1, 0)) == -1) {
       perror("Message Send Failed");
       exit(EXIT_FAILURE);
    }
 
+   //Receive formatted message from server
    qbuf.mtype = RCV_TYPE;
    if((msgrcv(qid, &qbuf, MAX_MSG_SIZE+10, RCV_TYPE, 0)) == -1) {
       perror("Did not receive formatted message");
       exit(EXIT_FAILURE);
    }
 
+   //output to STDOUT
    std::cout << std::string(qbuf.mtext); 
-
-   
-
-   //std::string test = "typing is propobaly the hardest thing absolutely charming and bragadocious willingly superfluous and meaningles to the point of excess";
-   //test = formatMessage(test);
-   //std::cout << test;
 
    return EXIT_SUCCESS;
 
