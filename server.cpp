b71531f24916e6a91a4bb9c40ac8933fa739c7e3
MBTree <31641097+MBTree@users.noreply.github.com>
2018-04-30 14:32:32 -0500

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -28,34 +28,35 @@ int main(int argc, char **argv) {
    struct mymsgbuf qbuf; 
 
 
-   key = ftok(".", 'q');
    //Create the message queue
+   key = ftok(".", 'q');
    if( (qid = msgget(key, IPC_CREAT | 0666)) == -1) {
       perror("Failed to create message queue");
       exit(EXIT_FAILURE);
    }
 
+   //Server waits indefinately until it receives a message
    while(1) {
       qbuf.mtype = RCV_TYPE;
       msgrcv(qid, &qbuf, MAX_MSG_SIZE, RCV_TYPE, 0);
 
+      //Format the message according to requirements, then prepare to send back
       std::string message = std::string(qbuf.mtext);
       message = formatMessage(message);
       strncpy(qbuf.mtext, message.c_str(), MAX_MSG_SIZE);
       qbuf.mtype = SND_TYPE;
 
+      //Send formatted message back to client
       if((msgsnd(qid, &qbuf, strlen(qbuf.mtext)+1, 0)) == -1) {
          perror("Message Send Failed");
          break;
       }
+      
    }
 
+   //Delete Message queue if there's an error
    msgctl(qid, IPC_RMID, 0);
-
-   //std::string test = "typing is propobaly the hardest thing absolutely charming and bragadocious willingly superfluous and meaningles to the point of excess";
-   //test = formatMessage(test);
-   //std::cout << test;
-
+   
    return EXIT_SUCCESS;
 
 }
@@ -64,17 +65,20 @@ std::string formatMessage(std::string message) {
 
    unsigned int lastNewLine = 0;
    for(unsigned int i = 79; i < message.length(); i--){
+      //Check if the 80th character on the line is not part of a word
+      //   if the 80th character is part of word, walk back before the word begins
       if(message[i] == ' '){
          message.replace(i,1, "\n");
          lastNewLine = i;
          i+=80;
-      } else if (i == lastNewLine){
+      } else if (i == lastNewLine){ //If the word is longer than 80 characters
          i+=80;
          lastNewLine = i;
          message.insert(i, "\n");
          i+=80;
       }
    }
+   //Add a newline at the end to make output look clean
    message.append("\n");
 
    return message; 
