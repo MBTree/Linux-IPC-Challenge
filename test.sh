8d1677d0b700e62400f315e09ece7dd0a98631e3
Michael Baum <mcb114@txstate.edu>
2018-04-30 14:50:03 -0500

    reworked test.sh

--- test.sh
+++ test.sh
@@ -8,6 +8,9 @@ sleep 1
 
 ./client "This test has over 500 characters and should truncate down to 500 characters but otherwise function normally. snake dog female tiki torch fans alligators papacy evangelicals theocracy Lorem ipsum dolor sit amet, diceret evertitur dissentias cu eam, dolor alienum vulputate ex per. In est nibh veri quidam. Ea sea agam rebum graecis, cu duo utamur disputando, petentium neglegentur comprehensam ne duo. Usu deleniti partiendo an. Per omnis putant i, vim eu tempor platonem vituperatoribus. ending now!############################### ##################### ##############"
 
-./client "having                         long                                  spaces                    in                  the                   text."
+for number in {0..1000}
+do
+./client >>file1.txt "having                         long                                  spaces                    in                  the                   text."
+done
 
-#killall -9 server
+killall -9 server
